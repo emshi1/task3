8a73598bdd51dbff9ca3de3f9232d92aa4d002d6
emshi1 <56219594+emshi1@users.noreply.github.com>
2020-11-05 01:19:33 +0700

    Updates

---
+++ precache-manifest.f2ef8b22098210c2acbe8e4278775c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ef9a6f27b2977aacd13006d9c110a9",
+    "url": "/task3/index.html"
+  },
+  {
+    "revision": "c944f3620c0605407a5b",
+    "url": "/task3/static/css/main.e7d33fbf.chunk.css"
+  },
+  {
+    "revision": "ee7571995310eaf55a92",
+    "url": "/task3/static/js/2.ebd7feba.chunk.js"
+  },
+  {
+    "revision": "7c8c498c154c5c7fc14cf0ad148613b4",
+    "url": "/task3/static/js/2.ebd7feba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c944f3620c0605407a5b",
+    "url": "/task3/static/js/main.48f5d936.chunk.js"
+  },
+  {
+    "revision": "b7b24e50651bfd62a83b",
+    "url": "/task3/static/js/runtime-main.689ebc3d.js"
+  }
+]);
