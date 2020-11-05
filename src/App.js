255f4fbd5c144676bf03dec1999deb2856fd978e
emshi1 <56219594+emshi1@users.noreply.github.com>
2020-11-05 18:22:10 +0700

    511

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from 'react';
 import TODOList from "./components/TODOList/TODOList"
 import Login from "./components/Login/LoginForm/LoginForm"
 import Registr from "./components/Login/Registr/Registr"
-import {Route, Switch} from "react-router-dom"
+import { Route, withRouter } from "react-router-dom"
 import Navbar from './components/Nav/Navbar'
 
+
+const todo = () => {
+  return (
+    < div >
+      <Navbar />
+      <Route path="/login" render={() => <Login />} />
+      <Route path="/" render={() => <TODOList />} />
+      <Route path="/Registr" render={() => <Registr />} />
+    </div >
+  )
+}
+
+const TodoWithRouter = withRouter(todo)
 
 const App = () => {
 
   return (
     <div className="App">
-      <Navbar/>
-      <Route  path ="/login" render = { () => <Login/>}/>
-      <Route  path ="/"render = { () => <TODOList/>}/>
-      <Route  path = "/Registr" render = { () => <Registr/>}/>
+      <TodoWithRouter />
     </div>
   );
 }
